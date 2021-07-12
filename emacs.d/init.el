c91d94eedc8b2403b789052a381df4b3a45db8b5
Masamitsu Oikawa <oicawa@gmail.com>
2021-07-12 11:45:00 +0900

    Update emacs init.el file. Update xkb files.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,7 +7,13 @@
 (setq-default truncate-lines t)
 ;; for separated window (left side & right side)
 (setq-default truncate-partial-width-windows t)
+;; Line number
+(global-linum-mode)
+(setq linum-format "%4d ")
+;; Current Line
+(global-hl-line-mode)
 
+;; Short cut keys
 (define-key global-map (kbd "H-h") 'backward-char)
 (define-key global-map (kbd "H-j") 'next-line)
 (define-key global-map (kbd "H-k") 'previous-line)
@@ -17,3 +23,40 @@
 (define-key global-map (kbd "H-C-j") 'forward-paragraph)
 (define-key global-map (kbd "H-C-k") 'backward-paragraph)
 (define-key global-map (kbd "H-C-l") 'forward-word)
+
+(define-key global-map (kbd "<zenkaku-hankaku>") 'toggle-input-method)
+
+(define-key global-map (kbd "H-b") 'backward-delete-char-untabify)
+(define-key global-map (kbd "H-d") 'delete-char)
+(define-key global-map (kbd "H-@") 'set-mark-command)
+
+; Line
+(define-key global-map (kbd "H-C-S-h") 'move-beginning-of-line)
+(define-key global-map (kbd "H-C-S-l") 'move-end-of-line)
+; Page
+(define-key global-map (kbd "H-S-j") 'scroll-down-command)
+(define-key global-map (kbd "H-S-k") 'scroll-up-command)
+; File
+(define-key global-map (kbd "H-C-l") 'beginning-of-buffer)
+(define-key global-map (kbd "H-C-l") 'end-of-buffer)
+
+; Copy / Cut /Paste
+(define-key global-map (kbd "H-c") 'kill-ring-save)
+(define-key global-map (kbd "H-x") 'kill-region)
+(define-key global-map (kbd "H-v") 'yank)
+
+; Cancel?
+(define-key global-map (kbd "H-e") 'keyboard-quit)
+
+; Save
+(define-key global-map (kbd "H-s") 'save-buffer)
+
+; Open
+(define-key global-map (kbd "H-o") 'find-file)
+
+; Undo
+(define-key global-map (kbd "H-z") 'undo)
+
+(define-key global-map (kbd "H-g") 'goto-line)
+
+(define-key global-map (kbd "H-m b") 'list-buffers)
--- xkb/xkbcomp4me.sh
+++ xkb/xkbcomp4me.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-xkbcomp -I$HOME/.xkb ~/.xkb/keymap/mykeymap $DISPLAY 2> /dev/null
+#xkbcomp -I$HOME/.xkb ~/.xkb/keymap/mykeymap $DISPLAY 2> /dev/null
+xkbcomp -I$HOME/.xkb ~/.xkb/keymap/mykeymap $DISPLAY 2> result.txt
+
